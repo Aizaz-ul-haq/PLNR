61c7269cc9e657ff31bf28d5569c33fc18bc7b20
Fatima Merger <f.hussain@micromerger.com>
2023-01-17 15:43:36 +0500

    top nav-bar

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -9,45 +9,6 @@ import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.componen
 import { FaqsComponent } from './faqs/faqs.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { InvestorCharterComponent } from './investor-charter/investor-charter.component';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzTabsModule } from 'ng-zorro-antd/tabs';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzSliderModule } from 'ng-zorro-antd/slider';
-import { NzDrawerModule } from 'ng-zorro-antd/drawer';
-import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzBadgeModule } from 'ng-zorro-antd/badge';
-import { NzMessageModule } from 'ng-zorro-antd/message';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzSpinModule } from 'ng-zorro-antd/spin';
-import { NzListModule } from 'ng-zorro-antd/list';
-import { NzAvatarModule } from 'ng-zorro-antd/avatar';
-import { NzStepsModule } from 'ng-zorro-antd/steps';
-import { NzNotificationModule } from 'ng-zorro-antd/notification';
-import { NzEmptyModule } from 'ng-zorro-antd/empty';
-import { NzPopoverModule } from 'ng-zorro-antd/popover';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzRadioModule } from 'ng-zorro-antd/radio';
-import { NzAffixModule } from 'ng-zorro-antd/affix';
-import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
-import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
-import { NzCalendarModule } from 'ng-zorro-antd/calendar';
-import { NzTagModule } from 'ng-zorro-antd/tag';
-import { NzPaginationModule } from 'ng-zorro-antd/pagination';
-import { NzUploadModule } from 'ng-zorro-antd/upload';
-import { NzRateModule } from 'ng-zorro-antd/rate';
-import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
-import { NzAlertModule } from 'ng-zorro-antd/alert';
-import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 
 
 
@@ -65,88 +26,13 @@ import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
   imports: [
     CommonModule,
     SharedRoutes,
-    NzGridModule,
-    NzLayoutModule,
-    NzIconModule,
-    NzMenuModule,
-    NzTabsModule,
-    NzInputModule,
-    NzButtonModule,
-    NzModalModule,
-    NzSliderModule,
-    NzDrawerModule,
-    NzDescriptionsModule,
-    NzTableModule,
-    NzDatePickerModule,
-    NzCardModule,
-    NzDropDownModule,
-    NzSelectModule,
-    NzBadgeModule,
-    NzMessageModule,
-    NzDividerModule,
-    NzSpinModule,
-    NzListModule,
-    NzAvatarModule,
-    NzStepsModule,
-    NzNotificationModule,
-    NzEmptyModule,
-    NzPopoverModule,
-    NzFormModule,
-    NzRadioModule,
-    NzAffixModule,
-    NzSkeletonModule,
-    NzCheckboxModule,
-    NzCalendarModule,
-    NzTagModule,
-    NzPaginationModule,
-    NzUploadModule,
-    NzRateModule,
-    NzTimePickerModule,
-    NzAlertModule,
-    NzInputNumberModule
+   
   ],
   exports: [
     FooterComponent,
     HomeModuleComponent,
     TopNavBarComponent,
-    NzGridModule,
-    NzIconModule,
-    NzMenuModule,
-    NzTabsModule,
-    NzInputModule,
-    NzButtonModule,
-    NzModalModule,
-    NzSliderModule,
-    NzDrawerModule,
-    NzDescriptionsModule,
-    NzTableModule,
-    NzDatePickerModule,
-    NzCardModule,
-    NzDropDownModule,
-    NzSelectModule,
-    NzBadgeModule,
-    NzMessageModule,
-    NzDividerModule,
-    NzSpinModule,
-    NzListModule,
-    NzAvatarModule,
-    NzStepsModule,
-    NzNotificationModule,
-    NzEmptyModule,
-    NzPopoverModule,
-    NzFormModule,
-    NzRadioModule,
-    NzAffixModule,
-    NzSkeletonModule,
-    NzCheckboxModule,
-    NzCalendarModule,
-    NzTagModule,
-    NzPaginationModule,
-    NzUploadModule,
-    NzRateModule,
-    NzTimePickerModule,
-    NzAlertModule,
-    NzInputNumberModule
+    
   ]
 })
 export class SharedModuleModule { }
