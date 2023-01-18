40d374f28ca1300ca59c776ffbaebe7a4c951743
Aizaz-ul-haq <aizazulhaq14@gmail.com>
2023-01-19 01:11:03 +0500

    Added all the pages.

--- src/app/shared-module/top-nav-bar/top-nav-bar.component.ts
+++ src/app/shared-module/top-nav-bar/top-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-nav-bar',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavBarComponent implements OnInit {
   hideNavbar: boolean = false;
+  isMobile: boolean = false;
+  isTab: boolean = false;
+  isToggled: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.checkMobile();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  @HostListener('window:load', ['$event'])
+  onEvent(event:any) {
+    this.checkMobile();
+  }
+  checkMobile() {
+    const windowWidth =
+      window.screen.width < window.outerWidth
+        ? window.screen.width
+        : window.outerWidth;
+    if (windowWidth < 480) {
+      this.isMobile = true;
+    } else if (windowWidth <= 1024 && windowWidth > 480) {
+      this.isTab = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
+  toggleMobileMenu() {
+    this.isToggled = !this.isToggled;
+  }
+  closeMobileMenu() {
+    console.log('what is happening; before: ', this.isToggled)
+    this.isToggled = false;
+    console.log('what is happening; After: ', this.isToggled)
+
   }
 
 }
