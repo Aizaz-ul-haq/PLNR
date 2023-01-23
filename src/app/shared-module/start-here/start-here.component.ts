a1fd401a20d4189c7c4367ddfb8270debfb4fd17
Aizaz-ul-haq <aizazulhaq14@gmail.com>
2023-01-23 16:11:52 +0500

    fixed stuff.

--- src/app/shared-module/start-here/start-here.component.ts
+++ src/app/shared-module/start-here/start-here.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-start-here',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-here.component.scss']
 })
 export class StartHereComponent implements OnInit {
+  constructor() { 
+    this.checkMobile();
 
-  constructor() { }
+  }
+  @HostListener('window:resize', ['$event'])
+  @HostListener('window:load', ['$event'])
+  onEvent(event:any) {
+    this.checkMobile();
+  }
+  isMobile: boolean = false;
+  isTab: boolean = false;
 
   ngOnInit(): void {
+  }
+  resize(){
+    this.checkMobile();
+  }
+
+  checkMobile() {
+    const windowWidth =
+      window.screen.width < window.outerWidth
+        ? window.screen.width
+        : window.outerWidth;
+    if (windowWidth < 480) {
+      this.isMobile = true;
+    } else if (windowWidth <= 1024 && windowWidth > 480) {
+      this.isTab = true;
+    } else {
+      this.isMobile = false;
+    }
   }
 
 }
