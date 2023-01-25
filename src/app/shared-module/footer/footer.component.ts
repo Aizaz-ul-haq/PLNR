9517b50d071f8bfd9c6cbb825517a4b4e4feae25
fatima merger <f.hussain@micromerger.com>
2023-01-25 16:27:30 +0500

    modal changes

--- src/app/shared-module/footer/footer.component.ts
+++ src/app/shared-module/footer/footer.component.ts
@@ -10,30 +10,22 @@ export class FooterComponent implements OnInit {
   
 @ViewChild('sayHelloTemplate', { read: TemplateRef }) sayHelloTemplate:TemplateRef<any> | undefined;
   seconds: any = 5;
+  showVideoModal:boolean = false;
   constructor(private modalService: NzModalService) { }
 
   ngOnInit(): void {
   
   }
   openSEBIModal(): void {
-    this.seconds = 5;
-    var interval = setInterval(() => {
-      this.seconds = this.seconds - 1;
-      return this.seconds;
-    }, 1000)
-    
-    const modal = this.modalService.success({
-      nzTitle: 'Regulatory Disclosure ',
-      nzContent: this.sayHelloTemplate
-    });
-    
+    this.showVideoModal = true; 
+  }
 
-    setTimeout(() => {
-      this.seconds = 5;
-      modal.destroy();
-      clearInterval(interval)
-    }, 50000);
-    
+  handleCancelTop(){
+    this.showVideoModal = false;
+  }
+
+  handleOkTop(){
+    this.showVideoModal = false;
   }
 
 }
